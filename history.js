52d12055c039526b16487b9e098eeeef6c221833
Naomi Owens <ndunhourowens@Naomis-MacBook-Pro.local>
2015-09-11 10:38:03 -1000

    first commit

---
+++ history.js
@@ -0,0 +1,18 @@
+var inputText = null;
+var historyText = null;
+
+window.onload = function() {
+  var ll = linkedListGenerator;
+
+  // button- add text to a node
+
+    // add node to the list
+
+
+
+
+
+  // button- display list
+
+    // remove list
+}
